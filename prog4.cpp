e1860d60dc540814efd6b75104a2b0baddf84495
Shivani Verma <64006038+Shivani-Verma786@users.noreply.github.com>
2020-05-26 16:05:10 +0530

    Add files via upload

---
+++ prog4.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class Dog
+{
+public:
+    int age;
+    string name, color, breed;
+    Dog();
+
+   Dog(int, string, string,string);
+    void displayDetails();
+};
+ Dog :: Dog()
+    { cout<<"Dog object created\n"; }
+
+Dog ::  Dog(int age, string name, string color, string breed)
+    {
+        this-> age = age;
+        this-> name = name;
+        this-> color = color;
+        this-> breed = breed;
+    }
+void Dog :: displayDetails()
+{
+    cout<<"age = "<<age<<endl<<"Name = "<<name<<endl<<"color = "<<color<<endl<<"breed = "<<breed<<"\n\n";
+}
+int main()
+{
+    Dog dog1(7,"jackie", "white","gold");
+    dog1.displayDetails();
+
+    Dog dog2(4, "Beagle", "Brown", "Pluto");
+    dog2.displayDetails();
+
+    return 0;
+}
